d81a9320579b4e710e867176c71641aecb1244cf
Devin Kenney <devin.kenney111@gmail.com>
2020-09-17 22:51:13 -0400

    added a function to check the winner

--- blackjack/solution.rb
+++ blackjack/solution.rb
@@ -25,13 +25,26 @@ class Card
   end
 end
 
+def check_winner player_value house_value
+
+  if player_value > house_value
+    return 'player'
+  elsif house_value > player_value
+    return 'house'
+  else
+    return 'tie'
+  end
+end
+
 (1..4).each do
   (2..9).each do |num|
     deck.append(Card.new num)
   end
-  (1..3).each do
+  (1..4).each do
     deck.append(Card.new 10)
   end
   deck.append(Card.new 11)
 end
+
+deck.shuffle!
 
