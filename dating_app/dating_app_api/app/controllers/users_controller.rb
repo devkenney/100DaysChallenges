43cb34a74bd1f3703d7aca1f6359f84c30c3aa06
Devin Kenney <devin.kenney111@gmail.com>
2020-09-21 20:06:35 -0400

    Shows three daters on each side of the main area

--- dating_app/dating_app_api/app/controllers/users_controller.rb
+++ dating_app/dating_app_api/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = User.all.sample(3)
 
     render json: @users
   end
--- dating_app/dating_app_client/src/App.js
+++ dating_app/dating_app_client/src/App.js
@@ -4,8 +4,15 @@ import Daters from './components/Daters.js';
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Daters />
+      <div className="parent">
+        <header><h1>SINGLE AND READY TO MINGLE HELL YEAH</h1></header>
+        <div className="main-body">
+          <aside><Daters /></aside>
+          <main></main>
+          <aside><Daters /></aside>
+        </div>
+        <footer><h3>This is a footer.</h3></footer>
+      </div>
     </div>
   )
 }
